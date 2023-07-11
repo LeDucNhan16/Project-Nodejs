cf335c8cc47cf340e24f220fc03d59f36d933aa2
Le Duc Nhan <nhandragon166@gmail.com>
2023-07-11 09:42:13 +0700

    remove puclic/scc folder

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "puclic")));
+
+app.use(morgan("combined"));
+
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/new", (req, res) => {
+  res.render("new");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
